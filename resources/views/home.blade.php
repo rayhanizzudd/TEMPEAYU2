a112ef75ee76560a0a1730722c3012f0b688a85d
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-23 12:32:27 +0700

    delete : profile mobile and web

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,33 +30,7 @@
             </div>
           </div>
         </div>
-        <div class="hidden md:block">
-          <div class="ml-4 flex items-center md:ml-6">
-            <!-- Profile dropdown -->
-            <div class="relative ml-3">
-              <div>
-                <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-yellow-500 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span class="absolute -inset-1.5"></span>
-                  <span class="sr-only">Open user menu</span>
-                  <img class="size-8 rounded-full" src="https://img.icons8.com/fluency-systems-regular/50/user-male-circle--v1.png" alt="">
-                </button>
-              </div> 
-              <div x-show="isOpen"
-                    x-transition:enter="transition ease-out duration-100 transform"
-                    x-transition:enter-start="opacity-0 scale-95"
-                    x-transition:enter-end="opacity-100 scale-100"
-                    x-transition:leave="transition ease-in duration-75 transform"
-                    x-transition:leave-start="opacity-100 scale-100"
-                    x-transition:leave-end="opacity-0 scale-95"
-
-                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                <a href="#" class="block px-4 py-2 text-sm text-black" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                <a href="#" class="block px-4 py-2 text-sm text-black" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                <a href="#" class="block px-4 py-2 text-sm text-black" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-              </div>
-            </div>
-          </div>
-        </div>
+          
         <div class="-mr-2 flex md:hidden">
           <button type="button" @click="isOpen = !isOpen" class="relative inline-flex items-center justify-center rounded-md bg-yellow-800 p-2 text-gray-400 hover:bg-yellow-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-800" aria-controls="mobile-menu" aria-expanded="false">
             <span class="absolute -inset-0.5"></span>
@@ -73,27 +47,12 @@
     </div>
 
     <div x-show='isOpen' class="md:hidden" id="mobile-menu">
-      <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+      <div class="flex flex-col space-y-1 px-2 pb-3 pt-2 sm:px-3 border border-white">
         <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-600 hover:text-black" aria-current="page">Dashboard</a>
         <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-600 hover:text-black">Product</a>
         <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-600 hover:text-black">Outlet</a>
         <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-yellow-600 hover:text-black">Location</a>
         {{-- <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a> --}}
-      </div>
-      <div class="border-t border-white pb-3 pt-4">
-        <div class="flex items-center px-5">
-          <div class="shrink-0">
-            <img class="size-10 rounded-full " src="https://img.icons8.com/fluency-systems-regular/50/user-male-circle--v1.png" alt="">
-          </div>
-          <div class="ml-3">
-            <div class="text-base/5 font-medium text-black">Ijujuju</div>
-          </div>
-        </div>
-        <div class="mt-3 space-y-1 px-2">
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-yellow-600 hover:text-black">Your Profile</a>
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-yellow-600 hover:text-black">Settings</a>
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-yellow-600 hover:text-black">Sign out</a>
-        </div>
       </div>
     </div>
   </nav>
