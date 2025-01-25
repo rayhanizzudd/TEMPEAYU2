86cc35ab9e1dd21476b3879fca85cac6d5509785
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-25 21:48:22 +0700

    update : home page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <div class="h-screen flex items-center justify-center bg-[url('https://s3-alpha-sig.figma.com/img/883c/0dbb/22a44184e35fe7d54cd0194cd75be229?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oB99yTtO74tLZhW0EEK32y~~Fh5Aq8u9ZvUZQ1EHtAP2oVW9m6Lk7yD-T0BNkMGPmZ55GqTsULq97ausRhM91V94A6jBFz4sWv-0BzZFSWbkrs5x7kB0h4BELWgL4gqL~gaGdO8ZkklFX3jxLkjGvLF5609xP22ERILqobo-aEBiTyUIwUVUp~Bu-RC509U9hY5a7KU8xnFYYhMx9Q7slHkyLwanp-JCV5KNaGVJqvTCcaSqvBRAzApmOem0cQcnAliHYRsuLwEXEOPuWTm6F-gWK-4aGeMkDq~5x1mmZHAH0ehIfmA4-M2HhJ3rxL-FpXeXbNCcqXiGH2QxPe0Q2w__')] bg-cover">
+    <div
+        class="h-screen flex items-center justify-center bg-[url('https://s3-alpha-sig.figma.com/img/883c/0dbb/22a44184e35fe7d54cd0194cd75be229?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oB99yTtO74tLZhW0EEK32y~~Fh5Aq8u9ZvUZQ1EHtAP2oVW9m6Lk7yD-T0BNkMGPmZ55GqTsULq97ausRhM91V94A6jBFz4sWv-0BzZFSWbkrs5x7kB0h4BELWgL4gqL~gaGdO8ZkklFX3jxLkjGvLF5609xP22ERILqobo-aEBiTyUIwUVUp~Bu-RC509U9hY5a7KU8xnFYYhMx9Q7slHkyLwanp-JCV5KNaGVJqvTCcaSqvBRAzApmOem0cQcnAliHYRsuLwEXEOPuWTm6F-gWK-4aGeMkDq~5x1mmZHAH0ehIfmA4-M2HhJ3rxL-FpXeXbNCcqXiGH2QxPe0Q2w__')] bg-cover">
         <div class=" px-4 py-6 sm:px-6 lg:px-8  w-full flex flex-col items-center justify-center gap-20">
             <div class="text-center  px-32 font-light">
                 <div class="text-6xl font-semibold">TEMPE</div> <br>
@@ -19,12 +20,13 @@
 
     <div class="h-screen bg-amber-200">
         <div class="px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center h-full">
-            <div class="text-center  w-2/5 h-full overflow-hidden">
-                <div class="p-4">
-                    <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
-                        alt="" class="w-full h-auto">
-                </div>
+            <div class="text-center w-2/5 h-full overflow-hidden">
+            <div class="p-4 h-full">
+                <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
+                    alt="Gambar Tempe" 
+                    class="w-full h-full max-w-full max-h-full border-2 border-black rounded-xl shadow-xl object-cover">
             </div>
+        </div>
             <div class="text-center  font-light w-3/5 h-full flex items-center flex-col justify-center">
                 <div class="text-4xl font-semibold px-32 text-start">Pilihan Sehat dari Kedelai Pilihan, Tempe Ayu</div>
                 <br><br>
@@ -48,24 +50,54 @@
                     berkomitmen untuk menghadirkan tempe murni yang alami dan kaya akan nutrisi bagi masyarakat
                     Indonesia dengan menghasilkan makanan yang tidak hanya lezat, tetapi juga sehat.</div>
             </div>
-            <div class="text-center  w-2/5 h-full overflow-hidden">
-                <div class="p-4">
-                    <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
-                        alt="" class="w-full h-auto">
-                </div>
+            <div class="text-center w-2/5 h-full overflow-hidden">
+            <div class="p-4 h-full">
+                <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
+                    alt="Gambar Tempe" 
+                    class="w-full h-full max-w-full max-h-full border-2 border-black rounded-xl shadow-xl object-cover">
             </div>
+        </div>
         </div>
     </div>
 
+    <div class="h-screen bg-amber-200">
+    <div class="px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center h-full">
+        <div class="text-center w-2/5 h-full overflow-hidden">
+            <div class="p-4 h-full">
+                <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
+                    alt="Gambar Tempe" 
+                    class="w-full h-full max-w-full max-h-full border-2 border-black rounded-xl shadow-xl object-cover">
+            </div>
+        </div>
 
-    <div class="h-screen bg-violet-300">
-        <div class=" px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center">
-            <div class="text-center font-light">
-                <div class="text-9xl">TEMPE AYU</div>
-                <div class="text-2xl font-medium">Tempe Sehat, Hiegenis, dan Original</div>
+        <div class="flex flex-col text-l font-medium gap-4 w-3/5">
+            <div class="text-center text-4xl font-semibold">Bagaimana cara mengetahui tempe yang berkualitas?</div>
+            <br>
+
+            <div class="text-start bg-amber-300 rounded-lg p-4 border-2 border-black border-solid">
+                Tempe yang baik memiliki warna putih merata. Warna ini berasal dari jamur Rhizopus yang tumbuh dengan baik.
+            </div>
+
+            <div class="text-ent bg-amber-300 rounded-lg p-4 border-2 border-black border-solid">
+                Tempe segar memiliki aroma khas. Jika tempe berbau asam atau busuk, sebaiknya tidak dikonsumsi.
+            </div>
+
+            <div class="text-start bg-amber-300 rounded-lg p-4 border-2 border-black border-solid">
+                Tempe yang baik harus padat dan keras. Tekstur yang terlalu lembek atau terlalu keras bisa menjadi indikasi bahwa tempe tidak dalam kondisi terbaik.
+            </div>
+
+            <div class="text-ent bg-amber-300 rounded-lg p-4 border-2 border-black border-solid">
+                Tempe yang berkualitas memiliki rasa yang enak dan gurih.
+            </div>
+
+            <div class="text-start bg-amber-300 rounded-lg p-4 border-2 border-black border-solid">
+                Tempe yang berkualitas baik mampu bertaham lama selama 7 hari dalam keadaan suhu ruang, dan 1 - 2 minggu dalam keadaan suhu kulkas.
             </div>
         </div>
     </div>
+</div>
+
+
 
 
 
