c51f07385561913147b338597f24ca1442ed660c
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-27 17:29:47 +0700

    update : home page add history and fact not fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <div
-        class="fade-in h-screen flex items-center justify-center bg-[url('https://s3-alpha-sig.figma.com/img/883c/0dbb/22a44184e35fe7d54cd0194cd75be229?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oB99yTtO74tLZhW0EEK32y~~Fh5Aq8u9ZvUZQ1EHtAP2oVW9m6Lk7yD-T0BNkMGPmZ55GqTsULq97ausRhM91V94A6jBFz4sWv-0BzZFSWbkrs5x7kB0h4BELWgL4gqL~gaGdO8ZkklFX3jxLkjGvLF5609xP22ERILqobo-aEBiTyUIwUVUp~Bu-RC509U9hY5a7KU8xnFYYhMx9Q7slHkyLwanp-JCV5KNaGVJqvTCcaSqvBRAzApmOem0cQcnAliHYRsuLwEXEOPuWTm6F-gWK-4aGeMkDq~5x1mmZHAH0ehIfmA4-M2HhJ3rxL-FpXeXbNCcqXiGH2QxPe0Q2w__')] bg-cover">
-        <div class=" px-4 py-6 sm:px-6 lg:px-8  w-full flex flex-col items-center justify-center gap-20">
+        class=" h-screen flex items-center justify-center bg-amber-200">
+        <div class="fade-in px-4 py-6 sm:px-6 lg:px-8  w-full flex flex-col items-center justify-center gap-20">
             <div class="text-center  px-32 font-light">
                 <div class="text-6xl font-semibold">TEMPE</div> <br>
                 <div class="text-l font-medium">Tempe adalah sumber protein nabati yang tinggi, menjadikannya alternatif
@@ -22,28 +22,79 @@
         </div>
     </div>
 
-    <div class="h-screen bg-amber-200">
+        <div class="h-screen p-20 ">
+        <div class="fade-in px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center h-full">
+            <div class="text-center  font-light w-3/5 h-full flex items-center flex-col justify-center">
+                <div class="text-4xl font-semibold px-32 text-start">Kandungan Tempe per 100 Gram</div>
+                <br><br>
+                <div class="text-xl px-20 text-justify">Tempe Ayu hadir sebagai produk tempe berkualitas
+                    yang terbuat dari kacang kedelai pilihan dan tanpa bahan pengawet atau bahan campuran lainnya, kami
+                    berkomitmen untuk menghadirkan tempe murni yang alami dan kaya akan nutrisi bagi masyarakat
+                    Indonesia dengan menghasilkan makanan yang tidak hanya lezat, tetapi juga sehat.</div>
+            </div>
+            <div class="text-center w-3/5 h-full overflow-hidden">
+                <div class="p-4  grid grid-cols-2 gap-4 h-full ">
+                    <div class="bg bg-amber-200 flex justify-center flex-col gap-2 rounded-xl shadow-sm ">
+                        <div class="text-5xl font-medium">13.8 Gram</div>
+                        <div class="text-xl ">Protein</div>
+                    </div>
+                    <div class="bg bg-amber-200 flex justify-center flex-col gap-2 rounded-xl shadow-sm">
+                        <div class="text-5xl font-medium">13.8 Gram</div>
+                        <div class="text-xl ">Protein</div>
+                    </div>
+                    <div class="bg bg-amber-200 flex justify-center flex-col gap-2 rounded-xl shadow-sm">
+                        <div class="text-5xl font-medium">13.8 Gram</div>
+                        <div class="text-xl ">Protein</div>
+                    </div>
+                    <div class="bg bg-amber-200 flex justify-center flex-col gap-2 rounded-xl shadow-sm">
+                        <div class="text-5xl font-medium">13.8 Gram</div>
+                        <div class="text-xl ">Protein</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div
+        class=" h-screen flex items-center justify-center bg-amber-200">
+        <div class="fade-in px-4 py-6 sm:px-6 lg:px-8  w-full flex flex-col items-center justify-center gap-20">
+            <div class="text-center  px-32 font-light">
+                <div class="text-6xl font-semibold">TEMPE</div> <br>
+                <div class="text-l font-medium">Tempe adalah sumber protein nabati yang tinggi, menjadikannya alternatif
+                    yang baik untuk pengganti daging bagi vegetarian dan vegan.</div>
+            </div>
+            <div class="text-center  px-60 font-light">
+                <div class="text-6xl font-semibold">TEMPE AYU</div> <br>
+                <div class="text-l font-medium">Tempe ayu merupakan Tempe Sehat, Hiegenis, dan Original yang diproduksi
+                    sejak tahun 19. Tempe Ayu dahulunya bernama Tempe Supari yang merupakan salah satu produksi tempe
+                    terbesar di wilayah Lumajang, Turun temurun Tempe Supari berubah menjadi Tempe Ayu. </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="h-screen p-20">
         <div class="fade-in px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center h-full">
             <div class="text-center w-2/5 h-full overflow-hidden">
                 <div class="p-4 h-full flex flex-col justify-center">
-                    <img src="https://s3-alpha-sig.figma.com/img/883c/0dbb/22a44184e35fe7d54cd0194cd75be229?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oB99yTtO74tLZhW0EEK32y~~Fh5Aq8u9ZvUZQ1EHtAP2oVW9m6Lk7yD-T0BNkMGPmZ55GqTsULq97ausRhM91V94A6jBFz4sWv-0BzZFSWbkrs5x7kB0h4BELWgL4gqL~gaGdO8ZkklFX3jxLkjGvLF5609xP22ERILqobo-aEBiTyUIwUVUp~Bu-RC509U9hY5a7KU8xnFYYhMx9Q7slHkyLwanp-JCV5KNaGVJqvTCcaSqvBRAzApmOem0cQcnAliHYRsuLwEXEOPuWTm6F-gWK-4aGeMkDq~5x1mmZHAH0ehIfmA4-M2HhJ3rxL-FpXeXbNCcqXiGH2QxPe0Q2w__"
-                        alt="Gambar Tempe"
-                        class="w-full h-3/5 max-w-full max-h-full ">
-                    <div class="text-6xl font-bold">Tempe Ayu -----</div>
+                    <img src="https://s3-alpha-sig.figma.com/img/5c95/60b4/898b8abdedaa19a2bfa3579a4802c18f?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ACaQV-0eceYHbtGt1nRxtv6zAaep97gzdqvs9DF50JcAPvIgVgjovzA-LJO0ayo5eF3y2Gg4n8oSyTa5BNhLCZxKrezUxoZLRzoCRtNSxIfj9kTLaLvQpY5H2-G2z4ybtseyKcI~-1bfLatGNEXvBxHCuJgg1~CvM~E9o-5zXkGhp6NibEYoG8PUQp56Kqnx9J2PnYnB8dAWSOpvf5MyVR97Ng7J9wMq1hwFwwJ8s1O1NpNTO6CobGpKGyEFVrrD0x0Oc2oOJtJQKMTSP2sJg39PhOTagOMmsqoPdiZDjoXw9TL7iQZDqzY2AXIsrqSMRAMoHQMOqk9sYCZBFH5L2A__"
+                        alt="Gambar Tempe" class="w-full h-3/5 max-w-full max-h-full ">
+                    <div class="text-5xl font-bold">Tempe Ayu---</div>
                 </div>
             </div>
             <div class="text-center  font-light w-3/5 h-full flex items-center flex-col justify-center">
-                <div class="text-4xl font-semibold px-32 text-start w-full ">Keunggulan</div>
+                <div class="text-4xl font-bold px-32 text-start w-full ">Keunggulan</div>
+                <div class="text-4xl font-bold px-32 text-start w-full ">______________</div>
                 <br><br>
-                <div class="text-xl font-semibold text-start px-36 w-full">Pilihan Sehat dari Kedelai Pilihan, Tempe Ayu</div>
-                <div class="text-m px-36 text-justify w-full">Tempe Ayu hadir sebagai produk tempe berkualitas
+                <div class="text-xl font-semibold text-start px-28 w-full">Pilihan Sehat dari Kedelai Pilihan, Tempe Ayu
+                </div>
+                <div class="text-m px-28 text-justify w-full">Tempe Ayu hadir sebagai produk tempe berkualitas
                     yang terbuat dari kacang kedelai pilihan dan tanpa bahan pengawet atau bahan campuran lainnya, kami
                     berkomitmen untuk menghadirkan tempe murni yang alami dan kaya akan nutrisi bagi masyarakat
                     Indonesia dengan menghasilkan makanan yang tidak hanya lezat, tetapi juga sehat.</div>
                 <br><br>
-                <div class="text-xl font-semibold text-start px-36 w-full ">Kualitas Terjaga, Cita Rasa
+                <div class="text-xl font-semibold text-start px-28 w-full ">Kualitas Terjaga, Cita Rasa
                     Tempe yang Tak Tertandingi</div>
-                <div class="text-m px-36 text-justify w-full">Setiap potong Tempe Ayu diproses dengan teliti melalui
+                <div class="text-m px-28 text-justify w-full">Setiap potong Tempe Ayu diproses dengan teliti melalui
                     penyortiran untuk menghasilkan tekstur yang lembut dan cita rasa khas yang mendalam. Kami menjaga
                     standar kebersihan dan kualitas pada setiap tahap produksi, sehingga pelanggan dapat menikmati tempe
                     yang segar, sehat, tahan lama, dan autentik.</div>
@@ -51,27 +102,6 @@
         </div>
     </div>
 
-
-
-    <div class="h-screen ">
-        <div class="fade-in px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-center h-full">
-            <div class="text-center  font-light w-3/5 h-full flex items-center flex-col justify-center">
-                <div class="text-4xl font-semibold px-32 text-start">Pilihan Sehat dari Kedelai Pilihan, Tempe Ayu</div>
-                <br><br>
-                <div class="text-xl font-medium px-36 text-justify">Tempe Ayu hadir sebagai produk tempe berkualitas
-                    yang terbuat dari kacang kedelai pilihan dan tanpa bahan pengawet atau bahan campuran lainnya, kami
-                    berkomitmen untuk menghadirkan tempe murni yang alami dan kaya akan nutrisi bagi masyarakat
-                    Indonesia dengan menghasilkan makanan yang tidak hanya lezat, tetapi juga sehat.</div>
-            </div>
-            <div class="text-center w-2/5 h-full overflow-hidden">
-                <div class="p-4 h-full">
-                    <img src="https://s3-alpha-sig.figma.com/img/7b35/524d/ddddf2fb92969ffe25cb47dd010bfcf9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMNBKqsgVsBoqayV-ez3hpHHbX9tku09cUeh249o~vfFOMJ5qtb-~LgaZ55oeblUmon5U~kobC3IUilmeywqH8x06oMD9aN2ojeN96LEs8AWs9LbtWGOzQ3L0CNZ5H3fr3KvI4GfK0g4-5jaOFpj8qjqU6Mwr61Algf~NdaamxATPwkfQfU8L9oefB1m8wYDymUH0IIToFjvH1naL5CDsZXYspR2EIf00PvN6Rs7kYEXvyAYAKJvWp4hbZZswUEhIzDMVUXd2jJJ1~DgS6MUOHsumceo194h5mzFwCeXvlYQLvg~A0xMD43pOQWvRSY6gdmZdYtU3BRfw4Hk--6IYg__"
-                        alt="Gambar Tempe"
-                        class="w-full h-full max-w-full max-h-full border-2 border-black rounded-xl shadow-xl object-cover">
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="h-screen bg-amber-200">
 
