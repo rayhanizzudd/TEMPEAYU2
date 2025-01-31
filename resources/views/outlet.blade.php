7472f7d8045acc223bab8d70429305e5e4b1a47c
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-31 19:27:29 +0700

    update : fix page outlet

--- resources/views/outlet.blade.php
+++ resources/views/outlet.blade.php
@@ -1,36 +1,156 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <div class="h-screen bg-amber-100">
-        <main class="p-32 grid grid-cols-2 gap-10 ">
-            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <div class="min-h-screen bg-amber-200 flex justify-center">
+        <main class="container mx-auto p-32 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 ">
+            <div class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="h-60 bg-red-200">
+                    <img src="https://s3-alpha-sig.figma.com/img/1d30/ea53/e2e8b73f7de8c8d6b8f5a128f72fb499?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=B7D9iySklf~7VvPK5nVJwf1XewkesTEKmeAVNC6~W2Y1hoDe~MCWRBFt1mgCIT~Sp4vXprtjrItSI2M7HFaWTqkFAquXcolLOyF9Dc3tpk370ERlVPxgPkWK9tJUzrnCRP513v8KJi9vmuzVZEsmxPambuRs4FlEzmqVuDwNj5-TTs4YG30xJw-W70KyoJUkWGYwRuesjBtNnlclw7Adz3TYy4AmVMx0ghpPExeDzS2VbN1xf8x46XisAZprIVlmJQglNKCJk6bhFt~lCUPeWTQwVb9JbFGau0ey2b-zOTjRNbr-8OibDiYFHl5SABRgCCpM1oeErcxmEcxggw2Dmw__" class="h-full w-full object-cover rounded-t-lg" alt="">
                 </div>
-            </a>
-            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Orion Lumajang</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Jl. Panglima Besar Sudirman Desa No.51, Tompokersan, Kec. Lumajang, Kabupaten Lumajang, Jawa Timur 67316
+                    </p>
+                    <a href="https://maps.app.goo.gl/WMQ7Zj8baPAtsRkE8" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                        Dapatkan Arah
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
                 </div>
-            </a>
-            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+            <div class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="h-60 bg-red-200">
+                    <img src="https://s3-alpha-sig.figma.com/img/84d4/8018/ad11dbc39216d193f223c12bbdf81039?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=YIAqR7xDGAMKbzHXmiRCik66ExcOC9K2iTyEhBWQFaFKXHQDhO6seZzg7rbz71IpaZAhwyAkb8JKqgLfl6OcqhrIdBkRc4CNCCo-FIeiFOht0sk7FYf7OnpAanQFgyOBZqDotwNwTqtCV~Ua-yRSxKQf0gbc7yJzYUKL0QC8VMS86E4tG9-0x0YOV6hWK03XltTpJnEZ1q4h7u2GFjCt3DPRV7-R~B~9pXZWteCF7is7mSN2rn1LeurQ~LLjyqiEwcrVDnrcrNahpAh2vBBQdcupFCM88KBHf0B5s6rdg2OmE4ze94Gb0mxrcLB4KUPQSCKDZdLS6hCb6WPQAsiqhQ__" class="h-full w-full object-cover rounded-t-lg" alt="">
                 </div>
-            </a>
-            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Juice Q'ta</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Jl. Letkol Slamet Wardoyo No.15, Citrodiwangsan, Kec. Lumajang, Kabupaten Lumajang, Jawa Timur 67316 
+                    </p>
+                    <a href="https://maps.app.goo.gl/rAfkACW1nmP8sdQk9" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                        Dapatkan Arah
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
                 </div>
-            </a>
+            </div>
+            <div class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class=" h-60 bg-red-200">
+                    <img src="https://s3-alpha-sig.figma.com/img/1ef1/cfe6/726bc2ca07148fee3390ce8e42ae37b6?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Rv5FEG5F1471kAsfPBQSeMBVPqkYi8OTz-Uyl3naaU4dr8FP2UJBMaL9SpD2w1yLBHGmObaz4QFi7v77hlwuwBc5DgTncM23-3mDtnMpjAVoiYwlcChXER2uTjJ1FASn~OhQ7C9T5tVmv9hEeo4j7onQCc7GeUIMRDPOdf4ZwKg2wvtYMyg~sls14sSoP6xtfJwEYDNb8xxv9ZFewfJ7j2WDXki~46hPL3HwrHzjoBh1q9cX9UdtyKV3q~S0Xe997DAEVeb-2MV~MR4FtanBJw0Ok6eV1mL2nM1IzaZ9yn4FfRlAmRd22~V0KfDAj6ccOhhnTmx7ta0Cv5DGB1S6Jg__" class="h-full w-full object-cover rounded-t-lg" alt="">
+                </div>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Toko Ibra</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Sumbersari, Penanggal, Kec. Candipuro, Kabupaten Lumajang, Jawa Timur 67373
+                    </p>
+                    <a href="https://maps.app.goo.gl/qVoTPBZujm6L5EG7A" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                        Dapatkan Arah
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class=" h-60 bg-red-200">
+                    <img src="https://s3-alpha-sig.figma.com/img/53c1/c925/f6c7dec73ab60c1170ce45b534de1c8c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nwKkYwXVfrluZx8RarK~oI~xNTQZC4r2k6i-56YCb52BeWJj~5eUuqRNcE12GUrrD3KjWADtv-o4DrmtzKh2r6CaOwQ2~EKb9vo7xGxKXs4W1nWY0sThMCpAOwIThchM3kSk7-3xS-75cLELVT8zjU6LMJrRkyN-YvIEvHApsbULJ823NVQVEF5b5sQdwGr~l6Ss28DGq7krNY0VC9zsV1cjchZ7lCUQx7i-V~Gdtox-~oxo289Hf7olO0SZ8uLVcVG7VORylTIlsVzR2cSAFz75mFtGjw5NYaJEI-1wGvcXK1J6V~G7WOwsxfp2M5KXF2css25XTSGCiGHM-eeJ2w__" class="h-full w-full object-cover rounded-t-lg" alt="">
+                </div>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Toko Djitu</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Sumberrejo, Sumberejo, Kec. Candipuro, Kabupaten Lumajang, Jawa Timur 67373 
+                    </p>
+                    <a href="https://maps.app.goo.gl/X14jWi4Y6AFXruiw6" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                        Dapatkan Arah
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            
+        </main>
+    </div>
+
+    <div class="h-screen bg-amber-100 flex justify-center items-center">
+        <main class=" fade-in flex flex-col w-full p-28">
+            <div class="flex flex-row gap-20">
+                <div class="w-full">
+                    <h1 class="text-black text-4xl font-bold">Bergabung dengan Kami</h1> <br>
+                    <h1 class="text-black text-l font-medium">Jl. Pancasila No.28, Krajan, Sumbermujur, Kec. Candipuro,
+                        Kabupaten Lumajang, Jawa Timur 67373</h1>
+                    <h1 class="text-black text-l font-medium">tempeayu28@gmail.com</h1>
+                    <h1 class="text-black text-l font-medium">082333991963</h1>
+                </div>
+
+                <div class="w-full">
+                    <form class="mx-auto w-full">
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="email" name="floating_email" id="floating_email"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_email"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama Outlet</label>
+                        </div>
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_first_name"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama
+                                    Depan</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_last_name" id="floating_last_name"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_last_name"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama
+                                    Belakang</label>
+                            </div>
+                        </div>
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="email" name="floating_email" id="floating_email"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_email"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alamat
+                                Email</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone"
+                                id="floating_phone"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_phone"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nomor
+                                Telepon</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-5 group">
+                            <textarea name="floating_large_text" id="floating_large_text" rows="4"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required></textarea>
+                            <label for="floating_large_text"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alamat Lengkap Outlet</label>
+                        </div>
+                        <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
+                </div>
+            </div>
+
         </main>
     </div>
 </x-layout>
