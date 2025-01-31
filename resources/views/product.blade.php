de948deefd7c84346ae7c88b93efef5445331d64
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-31 21:26:57 +0700

    update : fix product card color, and image

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,52 +1,47 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <div class="h-screen bg-amber-100 flex justify-center">
+    <div class="min-h-screen bg-amber-200 flex justify-center">
 
-        <main class="p-32 flex flex-col fade-in">
+        <main class="px-32 flex flex-col fade-in py-16">
             <div class="w-full text-center text-4xl font-medium">TEMPE AYU</div>
-            <div class="flex gap-20 mt-28">
+            <div class="container mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-14">
                 <div
                     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
+                    <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
+                        src="https://s3-alpha-sig.figma.com/img/539a/0fed/4384d6f05ed0eb403ee7ce0c8fbc5ad5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lhyU-Z-lQWXYAW2GT2-TPac8rTF5eTXQu1TKTk1C2CEGs3yrpbeLEm7H7qLbT7Hd2EuGDSYXRnB5AeUE9AEItFdpgIptO9LBtC2vwrPdDDw4IUazB3u5O1yt7Xm64xB9q~61rfropD1p48742yh0CGrgynQmQvWx7L9CVyE90nkKpibIpF8qaMcE2BPKqofjiOcm~nyWBytA0R-ON78zj8I6IPIIhnFWBnDiMNDtRkwi-K09phSnYljfZVVCn3kJxKwV6izj0YN4BSRBSiUhZ7D1VF3dfOvP~pH7bXFhbLyWqiGkBR-8it4fH9WL3dL7xcpsdbb0KjrCwLTwAi7v~g__"
+                        alt="product image" />
                     <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
+                        <p class="text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Tempe
+                            Premium</p>
+                        </p>
                         <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 10.000</span>
                         </div>
                     </div>
                 </div>
                 <div
                     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
+                    <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
+                        src="https://s3-alpha-sig.figma.com/img/420f/11bd/1a86b8f5cd1d8d65bf97439f6067839c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=byB00boGKFoRn9mYXw94-r5i-iitCJpbrK1xhXyDpS1HjwsiIMNmiiHh~l2M~34R~SEllN1RqZpIxBr-qy3amasC0ZdXeV1pFzqE4mxTkiNG4qMQH6cdIfcGzN1~y8zNa4ZBH3zX5zqRuTiRfM-NkZP3R6pR~QNm7njwon7Pd7tQfN-CI~43Kl1Q6lg~lU6Jl6olvSJEW89WTSA2Hy3kgk5kXwNP~Z2flmc-gHHhXleDpI0OQzHFrtLud1iT3WaWUYYaLrcLiJQw9~N30BoWANsj6ZS3lbXzWWcE0juzq98TbsPSumLHe8fXQrgxfFuEnilRq1sTWqyz79b-I6xFcQ__"
+                        alt="product image" />
                     <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
+                        <p class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Tempe
+                            Lonjong</p>
                         <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 5.000</span>
                         </div>
                     </div>
                 </div>
                 <div
                     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
+                    <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
+                        src="https://s3-alpha-sig.figma.com/img/7194/7764/342ad66482501b436352f07fd909c66b?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=abNDClDthg2CmfcZ1X8ecj0MqLD-RFALgaYrhcJleKY7o8FsRogq6DL9V-IuHW5x8kp~yQWp7RAuYQ2V1U6BFR1N3j-rKLZ2i4shr3KeXDcczroJPHGdqWqx1EJuS-mScBd70OsaUPS2pCvv45qgy~tpjaME5Ahvg0DFbg7AwE54eJHOZXNYwDL5StacvwJYMKqdf~HR9Q5UlXFlvtda1P0QkkYVbmFDRj4V0smnfRW3L5ggSbv-wPY4vunGNzw6AULl64srYioIXHxIJD7NYWlN4b86JP~1NHbWBPc6N3EjkEExdJySbx3e~DR4t3ACO-zKLerPM51XeB~GRyUazg__"
+                        alt="product image" />
                     <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
+                        <p class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Tempe
+                            Kotak</p>
                         <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 3.000</span>
                         </div>
                     </div>
                 </div>
@@ -54,52 +49,35 @@
         </main>
     </div>
 
-    <div class="h-screen bg-amber-300">
-        <main class="p-32 flex flex-col fade-in">
+    <div class="min-h-screen bg-amber-100 flex justify-center">
+        <main class="px-32 flex flex-col fade-in py-16">
             <div class="w-full text-center text-4xl font-medium">KERING TEMPE AYU</div>
-            <div class="flex gap-20 mt-28">
+            <div class="container mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 mt-14">
                 <div
                     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
+                    <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
+                        src="https://s3-alpha-sig.figma.com/img/539a/0fed/4384d6f05ed0eb403ee7ce0c8fbc5ad5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lhyU-Z-lQWXYAW2GT2-TPac8rTF5eTXQu1TKTk1C2CEGs3yrpbeLEm7H7qLbT7Hd2EuGDSYXRnB5AeUE9AEItFdpgIptO9LBtC2vwrPdDDw4IUazB3u5O1yt7Xm64xB9q~61rfropD1p48742yh0CGrgynQmQvWx7L9CVyE90nkKpibIpF8qaMcE2BPKqofjiOcm~nyWBytA0R-ON78zj8I6IPIIhnFWBnDiMNDtRkwi-K09phSnYljfZVVCn3kJxKwV6izj0YN4BSRBSiUhZ7D1VF3dfOvP~pH7bXFhbLyWqiGkBR-8it4fH9WL3dL7xcpsdbb0KjrCwLTwAi7v~g__"
+                        alt="product image" />
                     <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
+                        <p class="text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Tempe
+                            Premium</p>
+                        </p>
                         <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 10.000</span>
                         </div>
                     </div>
                 </div>
                 <div
                     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
+                    <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
+                        src="https://s3-alpha-sig.figma.com/img/420f/11bd/1a86b8f5cd1d8d65bf97439f6067839c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=byB00boGKFoRn9mYXw94-r5i-iitCJpbrK1xhXyDpS1HjwsiIMNmiiHh~l2M~34R~SEllN1RqZpIxBr-qy3amasC0ZdXeV1pFzqE4mxTkiNG4qMQH6cdIfcGzN1~y8zNa4ZBH3zX5zqRuTiRfM-NkZP3R6pR~QNm7njwon7Pd7tQfN-CI~43Kl1Q6lg~lU6Jl6olvSJEW89WTSA2Hy3kgk5kXwNP~Z2flmc-gHHhXleDpI0OQzHFrtLud1iT3WaWUYYaLrcLiJQw9~N30BoWANsj6ZS3lbXzWWcE0juzq98TbsPSumLHe8fXQrgxfFuEnilRq1sTWqyz79b-I6xFcQ__"
+                        alt="product image" />
                     <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
+                        <p class="text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Tempe
+                            Premium</p>
+                        </p>
                         <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-                    </a>
-                    <div class="px-5 pb-5">
-                        <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch
-                                Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                        </a>
-                        <div class="flex items-center justify-center">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 10.000</span>
                         </div>
                     </div>
                 </div>
