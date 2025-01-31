30adb5da5dde674bb165271c5c995212cceccabe
rayhanizzudd <rayhannavis11@gmail.com>
2025-01-31 21:30:02 +0700

    update : fix product card color

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,7 +6,7 @@
             <div class="w-full text-center text-4xl font-medium">TEMPE AYU</div>
             <div class="container mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-14">
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-yellow-600 hover:dark:bg-yellow-700">
                     <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
                         src="https://s3-alpha-sig.figma.com/img/539a/0fed/4384d6f05ed0eb403ee7ce0c8fbc5ad5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lhyU-Z-lQWXYAW2GT2-TPac8rTF5eTXQu1TKTk1C2CEGs3yrpbeLEm7H7qLbT7Hd2EuGDSYXRnB5AeUE9AEItFdpgIptO9LBtC2vwrPdDDw4IUazB3u5O1yt7Xm64xB9q~61rfropD1p48742yh0CGrgynQmQvWx7L9CVyE90nkKpibIpF8qaMcE2BPKqofjiOcm~nyWBytA0R-ON78zj8I6IPIIhnFWBnDiMNDtRkwi-K09phSnYljfZVVCn3kJxKwV6izj0YN4BSRBSiUhZ7D1VF3dfOvP~pH7bXFhbLyWqiGkBR-8it4fH9WL3dL7xcpsdbb0KjrCwLTwAi7v~g__"
                         alt="product image" />
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-yellow-600 hover:dark:bg-yellow-700">
                     <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
                         src="https://s3-alpha-sig.figma.com/img/420f/11bd/1a86b8f5cd1d8d65bf97439f6067839c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=byB00boGKFoRn9mYXw94-r5i-iitCJpbrK1xhXyDpS1HjwsiIMNmiiHh~l2M~34R~SEllN1RqZpIxBr-qy3amasC0ZdXeV1pFzqE4mxTkiNG4qMQH6cdIfcGzN1~y8zNa4ZBH3zX5zqRuTiRfM-NkZP3R6pR~QNm7njwon7Pd7tQfN-CI~43Kl1Q6lg~lU6Jl6olvSJEW89WTSA2Hy3kgk5kXwNP~Z2flmc-gHHhXleDpI0OQzHFrtLud1iT3WaWUYYaLrcLiJQw9~N30BoWANsj6ZS3lbXzWWcE0juzq98TbsPSumLHe8fXQrgxfFuEnilRq1sTWqyz79b-I6xFcQ__"
                         alt="product image" />
@@ -33,7 +33,7 @@
                     </div>
                 </div>
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-yellow-600 hover:dark:bg-yellow-700">
                     <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
                         src="https://s3-alpha-sig.figma.com/img/7194/7764/342ad66482501b436352f07fd909c66b?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=abNDClDthg2CmfcZ1X8ecj0MqLD-RFALgaYrhcJleKY7o8FsRogq6DL9V-IuHW5x8kp~yQWp7RAuYQ2V1U6BFR1N3j-rKLZ2i4shr3KeXDcczroJPHGdqWqx1EJuS-mScBd70OsaUPS2pCvv45qgy~tpjaME5Ahvg0DFbg7AwE54eJHOZXNYwDL5StacvwJYMKqdf~HR9Q5UlXFlvtda1P0QkkYVbmFDRj4V0smnfRW3L5ggSbv-wPY4vunGNzw6AULl64srYioIXHxIJD7NYWlN4b86JP~1NHbWBPc6N3EjkEExdJySbx3e~DR4t3ACO-zKLerPM51XeB~GRyUazg__"
                         alt="product image" />
@@ -54,7 +54,7 @@
             <div class="w-full text-center text-4xl font-medium">KERING TEMPE AYU</div>
             <div class="container mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 mt-14">
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-yellow-600 hover:dark:bg-yellow-700">
                     <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
                         src="https://s3-alpha-sig.figma.com/img/539a/0fed/4384d6f05ed0eb403ee7ce0c8fbc5ad5?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lhyU-Z-lQWXYAW2GT2-TPac8rTF5eTXQu1TKTk1C2CEGs3yrpbeLEm7H7qLbT7Hd2EuGDSYXRnB5AeUE9AEItFdpgIptO9LBtC2vwrPdDDw4IUazB3u5O1yt7Xm64xB9q~61rfropD1p48742yh0CGrgynQmQvWx7L9CVyE90nkKpibIpF8qaMcE2BPKqofjiOcm~nyWBytA0R-ON78zj8I6IPIIhnFWBnDiMNDtRkwi-K09phSnYljfZVVCn3kJxKwV6izj0YN4BSRBSiUhZ7D1VF3dfOvP~pH7bXFhbLyWqiGkBR-8it4fH9WL3dL7xcpsdbb0KjrCwLTwAi7v~g__"
                         alt="product image" />
@@ -68,7 +68,7 @@
                     </div>
                 </div>
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-yellow-600 hover:dark:bg-yellow-700">
                     <img class="p-8 rounded-t-lg h-[30rem]  w-full object-cover"
                         src="https://s3-alpha-sig.figma.com/img/420f/11bd/1a86b8f5cd1d8d65bf97439f6067839c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=byB00boGKFoRn9mYXw94-r5i-iitCJpbrK1xhXyDpS1HjwsiIMNmiiHh~l2M~34R~SEllN1RqZpIxBr-qy3amasC0ZdXeV1pFzqE4mxTkiNG4qMQH6cdIfcGzN1~y8zNa4ZBH3zX5zqRuTiRfM-NkZP3R6pR~QNm7njwon7Pd7tQfN-CI~43Kl1Q6lg~lU6Jl6olvSJEW89WTSA2Hy3kgk5kXwNP~Z2flmc-gHHhXleDpI0OQzHFrtLud1iT3WaWUYYaLrcLiJQw9~N30BoWANsj6ZS3lbXzWWcE0juzq98TbsPSumLHe8fXQrgxfFuEnilRq1sTWqyz79b-I6xFcQ__"
                         alt="product image" />
