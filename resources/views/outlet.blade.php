f4253f31c202bee2fda160c45821fb3b3859ace0
rayhanizzudd <rayhannavis11@gmail.com>
2025-04-04 22:49:04 +0700

    update : image outlet

--- resources/views/outlet.blade.php
+++ resources/views/outlet.blade.php
@@ -8,7 +8,7 @@
                 <div
                     class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-yellow-600 dark:border-gray-700">
                     <div class="h-60 ">
-                        <img src="https://s3-alpha-sig.figma.com/img/1d30/ea53/e2e8b73f7de8c8d6b8f5a128f72fb499?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=B7D9iySklf~7VvPK5nVJwf1XewkesTEKmeAVNC6~W2Y1hoDe~MCWRBFt1mgCIT~Sp4vXprtjrItSI2M7HFaWTqkFAquXcolLOyF9Dc3tpk370ERlVPxgPkWK9tJUzrnCRP513v8KJi9vmuzVZEsmxPambuRs4FlEzmqVuDwNj5-TTs4YG30xJw-W70KyoJUkWGYwRuesjBtNnlclw7Adz3TYy4AmVMx0ghpPExeDzS2VbN1xf8x46XisAZprIVlmJQglNKCJk6bhFt~lCUPeWTQwVb9JbFGau0ey2b-zOTjRNbr-8OibDiYFHl5SABRgCCpM1oeErcxmEcxggw2Dmw__"
+                        <img src="assets/orion.png"
                             class="h-full w-full object-cover rounded-t-lg" alt="">
                     </div>
                     <div class="p-5">
@@ -34,7 +34,7 @@
                 <div
                     class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-yellow-600 dark:border-gray-700">
                     <div class="h-60 ">
-                        <img src="https://s3-alpha-sig.figma.com/img/84d4/8018/ad11dbc39216d193f223c12bbdf81039?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=YIAqR7xDGAMKbzHXmiRCik66ExcOC9K2iTyEhBWQFaFKXHQDhO6seZzg7rbz71IpaZAhwyAkb8JKqgLfl6OcqhrIdBkRc4CNCCo-FIeiFOht0sk7FYf7OnpAanQFgyOBZqDotwNwTqtCV~Ua-yRSxKQf0gbc7yJzYUKL0QC8VMS86E4tG9-0x0YOV6hWK03XltTpJnEZ1q4h7u2GFjCt3DPRV7-R~B~9pXZWteCF7is7mSN2rn1LeurQ~LLjyqiEwcrVDnrcrNahpAh2vBBQdcupFCM88KBHf0B5s6rdg2OmE4ze94Gb0mxrcLB4KUPQSCKDZdLS6hCb6WPQAsiqhQ__"
+                        <img src="assets/qta.png"
                             class="h-full w-full object-cover rounded-t-lg" alt="">
                     </div>
                     <div class="p-5">
@@ -60,18 +60,18 @@
                 <div
                     class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-yellow-600 dark:border-gray-700">
                     <div class=" h-60 ">
-                        <img src="https://s3-alpha-sig.figma.com/img/1ef1/cfe6/726bc2ca07148fee3390ce8e42ae37b6?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Rv5FEG5F1471kAsfPBQSeMBVPqkYi8OTz-Uyl3naaU4dr8FP2UJBMaL9SpD2w1yLBHGmObaz4QFi7v77hlwuwBc5DgTncM23-3mDtnMpjAVoiYwlcChXER2uTjJ1FASn~OhQ7C9T5tVmv9hEeo4j7onQCc7GeUIMRDPOdf4ZwKg2wvtYMyg~sls14sSoP6xtfJwEYDNb8xxv9ZFewfJ7j2WDXki~46hPL3HwrHzjoBh1q9cX9UdtyKV3q~S0Xe997DAEVeb-2MV~MR4FtanBJw0Ok6eV1mL2nM1IzaZ9yn4FfRlAmRd22~V0KfDAj6ccOhhnTmx7ta0Cv5DGB1S6Jg__"
+                        <img src="assets/asy.png"
                             class="h-full w-full object-cover rounded-t-lg" alt="">
                     </div>
                     <div class="p-5">
                         <p>
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Toko Ibra
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Asy Mart
                         </h5>
                         </p>
                         <p class="mb-3 font-normal text-white dark:text-white">
-                            Sumbersari, Penanggal, Kec. Candipuro, Kabupaten Lumajang, Jawa Timur 67373
+                            Jl. Citandui No.7, Jogoyudan, Kec. Lumajang, Kabupaten Lumajang, Jawa Timur 67316
                         </p>
-                        <a href="https://maps.app.goo.gl/qVoTPBZujm6L5EG7A" target="_blank"
+                        <a href="https://maps.app.goo.gl/ta8Bte1BwVPxzPS17" target="_blank"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800">
                             Dapatkan Arah
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg"
@@ -85,18 +85,18 @@
                 <div
                     class="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-yellow-600 dark:border-gray-700">
                     <div class=" h-60 ">
-                        <img src="https://s3-alpha-sig.figma.com/img/53c1/c925/f6c7dec73ab60c1170ce45b534de1c8c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nwKkYwXVfrluZx8RarK~oI~xNTQZC4r2k6i-56YCb52BeWJj~5eUuqRNcE12GUrrD3KjWADtv-o4DrmtzKh2r6CaOwQ2~EKb9vo7xGxKXs4W1nWY0sThMCpAOwIThchM3kSk7-3xS-75cLELVT8zjU6LMJrRkyN-YvIEvHApsbULJ823NVQVEF5b5sQdwGr~l6Ss28DGq7krNY0VC9zsV1cjchZ7lCUQx7i-V~Gdtox-~oxo289Hf7olO0SZ8uLVcVG7VORylTIlsVzR2cSAFz75mFtGjw5NYaJEI-1wGvcXK1J6V~G7WOwsxfp2M5KXF2css25XTSGCiGHM-eeJ2w__"
+                        <img src="assets/tosabu.png"
                             class="h-full w-full object-cover rounded-t-lg" alt="">
                     </div>
                     <div class="p-5">
                         <p>
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Toko Djitu
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TOSABU
                         </h5>
                         </p>
                         <p class="mb-3 font-normal text-white dark:text-white">
-                            Sumberrejo, Sumberejo, Kec. Candipuro, Kabupaten Lumajang, Jawa Timur 67373
+                            Jl. Gajah Mada No.10a, Kepuharjo, Kec. Lumajang, Kabupaten Lumajang, Jawa Timur 67316
                         </p>
-                        <a href="https://maps.app.goo.gl/X14jWi4Y6AFXruiw6" target="_blank"
+                        <a href="https://maps.app.goo.gl/UTHZhC7X6RZEMxyN9" target="_blank"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800">
                             Dapatkan Arah
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg"
